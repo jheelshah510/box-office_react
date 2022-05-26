aa7f814a7ebe15a8c309087f4473ebb5dba46142
Jheel Shah <jheelshah510@gmail.com>
2022-05-26 16:37:24 +0530

    useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,33 @@
-import React, { useState } from 'react';
-import MainPageLayout from '../components/MainPageLayout';
-import { apiGet } from '../misc/config';
-import ShowGrid from '../components/show/ShowGrid';
-import ActorGrid from '../components/actor/ActorGrid';
+import React, { useState } from "react";
+import MainPageLayout from "../components/MainPageLayout";
+import { apiGet } from "../misc/config";
+import ShowGrid from "../components/show/ShowGrid";
+import ActorGrid from "../components/actor/ActorGrid";
 
 const Home = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [results, setResults] = useState(null);
-  const [searchOption, setSearchOption] = useState('shows');
+  const [searchOption, setSearchOption] = useState("shows");
 
-  const isShowsSearch = searchOption === 'shows';
+  const isShowsSearch = searchOption === "shows";
 
   const onSearch = () => {
-    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+    apiGet(`/search/${searchOption}?q=${input}`).then((result) => {
       setResults(result);
     });
   };
 
-  const onInputChange = ev => {
+  const onInputChange = (ev) => {
     setInput(ev.target.value);
   };
 
-  const onKeyDown = ev => {
+  const onKeyDown = (ev) => {
     if (ev.keyCode === 13) {
       onSearch();
     }
   };
 
-  const onRadioChange = ev => {
+  const onRadioChange = (ev) => {
     setSearchOption(ev.target.value);
   };
 
